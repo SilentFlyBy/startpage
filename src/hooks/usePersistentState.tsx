74c2d9d61acfab1aef21094d8c04a962cf71cae9
Erik Moldtmann <erik@moldtmann.de>
2021-02-13 21:27:07 +0100

    fix build

--- src/hooks/usePersistentState.tsx
+++ src/hooks/usePersistentState.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 
+const windowDefined = typeof window !== 'undefined';
+
 export const usePersistentState = (defaultValue: any, key: any) => {
   const [value, setValue] = useState(() => {
-    const stickyValue = window.localStorage.getItem(key);
+    const stickyValue = windowDefined ? window.localStorage.getItem(key) : null;
     return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
   });
   useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(value));
+    windowDefined && window.localStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
   return [value, setValue];
 };
