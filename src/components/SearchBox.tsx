8e8d4ba2aad12047d1a60e38a09c7bf0701498c2
Erik Moldtmann <erik@moldtmann.de>
2021-02-14 12:30:41 +0100

    Adjust searchbox

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -7,7 +7,13 @@ interface Props {
   style?: React.CSSProperties;
 }
 
-const searchEngines = [
+interface SearchEngine {
+  name: string;
+  action: string;
+  query: string;
+}
+
+const searchEngines: SearchEngine[] = [
   { name: 'Google', action: 'http://www.google.com/search', query: 'q' },
   { name: 'DuckDuckGo', action: 'https://duckduckgo.com/', query: 'q' },
   {
@@ -17,53 +23,69 @@ const searchEngines = [
   },
 ];
 
-export const SearchBox = ({ className, style }: Props) => {
+const SearchEngineDropdown = ({
+  searchEngine,
+  onSetSearchEngine,
+}: {
+  searchEngine: SearchEngine;
+  onSetSearchEngine: (search: SearchEngine) => any;
+}) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  return (
+    <div
+      className={`relative py-1 px-2 border-r bg-gradient-to-t from-gray-100 to-white rounded-l ${
+        dropdownOpen && 'rounded-bl-none'
+      }`}
+    >
+      <div
+        className=" flex text-gray-500 hover:text-gray-300 transition-colors cursor-pointer"
+        onClick={() => setDropdownOpen(!dropdownOpen)}
+      >
+        <span>{searchEngine.name}</span>
+        <DropdownArrow className="block ml-1" style={{ width: '10px' }} />
+      </div>
+      {dropdownOpen && (
+        <div className="absolute bg-white rounded-b top-8 min-w-full py-1 left-0 shadow">
+          <ul>
+            {searchEngines.map(search => (
+              <li
+                key={search.name}
+                className="px-2 hover:bg-gray-500 hover:text-white hover:italic cursor-pointer"
+                onClick={() => {
+                  onSetSearchEngine(search);
+                  setDropdownOpen(false);
+                }}
+              >
+                {search.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export const SearchBox = ({ className, style }: Props) => {
   const [searchEngine, setSearchEngine] = usePersistentState(
     searchEngines[0],
     'searchEngine',
   );
   return (
-    <div
-      className={`flex bg-white mx-auto rounded ${
-        dropdownOpen && 'rounded-bl-none'
-      } ${className}`}
-      style={style}
-    >
-      <div className="relative py-1 px-2 border-r bg-gradient-to-t from-gray-100 to-white rounded-l">
-        <div
-          className=" flex text-gray-500 hover:text-gray-300 transition-colors cursor-pointer"
-          onClick={() => setDropdownOpen(!dropdownOpen)}
-        >
-          <span>{searchEngine.name}</span>
-          <DropdownArrow className="block ml-1" style={{ width: '10px' }} />
-        </div>
-        {dropdownOpen && (
-          <div className="absolute bg-white rounded-b top-8 min-w-full py-1 left-0 shadow">
-            <ul>
-              {searchEngines.map(search => (
-                <li
-                  key={search.name}
-                  className="px-2 hover:bg-gray-500 hover:text-white hover:italic cursor-pointer"
-                  onClick={() => {
-                    setSearchEngine(search);
-                    setDropdownOpen(false);
-                  }}
-                >
-                  {search.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-      <div className="py-1 px-2 flex-grow">
+    <div className={`flex mx-auto ${className}`} style={style}>
+      <SearchEngineDropdown
+        searchEngine={searchEngine}
+        onSetSearchEngine={search => setSearchEngine(search)}
+      />
+      <div className="py-1 px-2 flex-grow bg-white rounded-r">
         <form action={searchEngine.action} method="get" target="_blank">
           <input
             type="text"
             placeholder="Suche"
             className="block w-full focus:outline-none"
             name={searchEngine.query}
+            autoFocus
           ></input>
         </form>
       </div>
